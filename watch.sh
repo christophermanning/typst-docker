11c4f19db0352384f043c39dc4750f1d3c3a2b1c
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-05-01 16:16:03 -0500

    watch.sh: update output formatting

--- watch.sh
+++ watch.sh
@@ -6,10 +6,13 @@ typst fonts
 
 # compile typst files on launch
 echo -e "\n# Compiling Files"
-ls *.typ
-typst compile *.typ
+for file in *.typ; do
+  echo -n "$file "
+  typst compile $file
+  echo -e "\xE2\x9C\x94"
+done
 
 # incrementally compile changed files
 # use poll_monitor because it's more reliable than inotify in docker mounted volumes
 echo -e "\n# Watching $(pwd)/*.typ for Changes"
-fswatch -0 --monitor=poll_monitor *.typ | xargs -0 -n1 -I{} echo "Compiling {}" && typst compile "{}"
+fswatch -0 --monitor=poll_monitor *.typ | xargs -0 -n1 -I{} sh -c 'echo -n "Compiling {} " && typst compile "{}" && echo -e "\xE2\x9C\x94"'
