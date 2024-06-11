e1496f11b30890e99a3a781fef752478fdaa7c30
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-06-11 16:46:29 -0500

    watch.sh: add compile function

--- watch.sh
+++ watch.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 set -euo pipefail
 
+function typst_compile() {
+  file=$1
+  echo -n "Compiling $file "
+  typst compile $file
+  echo -e "\xE2\x9C\x94"
+}
+
+# if an input argument is provided, just compile the requested file
+if [ $# -ge 1 ]; then
+  typst_compile $1
+  exit 0
+fi
+
 echo -e "# Available Fonts"
 typst fonts
 
 # compile typst files on launch
 echo -e "\n# Compiling Files"
 for file in $(find . -name '*.typ'); do
-  echo -n "$file "
-  typst compile $file
-  echo -e "\xE2\x9C\x94"
+  typst_compile $file
 done
+
+# watch all directories with *.typ files
+dirs=$(find . -name '*.typ' -exec dirname "{}" \; | sort -u | sed 's/.*/\0\/*.typ/' | paste -sd' ')
+echo -e "\n# Watching $dirs for Changes"
 
 # incrementally compile changed files
 # use poll_monitor because it's more reliable than inotify in docker mounted volumes
-dirs=$(find . -name '*.typ' -exec dirname "{}" \; | sort -u | sed 's/.*/\0\/*.typ/' | paste -sd' ')
-echo -e "\n# Watching $dirs for Changes"
-fswatch -0 --monitor=poll_monitor $dirs | xargs -0 -n1 -I{} sh -c 'echo -n "Compiling {} " && typst compile "{}" && echo -e "\xE2\x9C\x94"'
+fswatch -0 --monitor=poll_monitor $dirs | xargs -0 -n1 -I{} sh -c 'watch.sh {}'
