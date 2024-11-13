453e01c9c723a8973859f8dcc46b838a0f745bc5
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-11-13 15:40:16 -0600

    watch.sh: add text formatting

--- watch.sh
+++ watch.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -euo pipefail
 
+NO_FORMAT="\033[0m"
+UNDERLINE="\033[4m"
+BOLD="\033[1m"
+GREEN="\033[38;5;2m"
+
 function typst_compile() {
   file=$1
   echo -n "Compiling $file "
   typst compile $file
-  echo -e "\xE2\x9C\x94"
+  echo -e "${GREEN}\xE2\x9C\x94${NO_FORMAT}"
 }
 
 # if an input argument is provided, just compile the requested file
@@ -14,18 +19,18 @@ if [ $# -ge 1 ]; then
   exit 0
 fi
 
-echo -e "# Available Fonts"
+echo -e "${BOLD}${UNDERLINE}Available Fonts${NO_FORMAT}"
 typst fonts
 
 # compile typst files on launch
-echo -e "\n# Compiling Files"
+echo -e "\n${BOLD}${UNDERLINE}Compiling Files${NO_FORMAT}"
 for file in $(find . -name '*.typ'); do
   typst_compile $file
 done
 
 # watch all directories with *.typ files
 dirs=$(find . -name '*.typ' -exec dirname "{}" \; | sort -u | sed 's/.*/\0\/*.typ/' | paste -sd' ')
-echo -e "\n# Watching $dirs for Changes"
+echo -e "\n${BOLD}${UNDERLINE}Watching $dirs for Changes${NO_FORMAT}"
 
 # incrementally compile changed files
 # use poll_monitor because it's more reliable than inotify in docker mounted volumes
